a9c5bbd2afe35e3754a978610676126f914178c6
TeeMeeLehin <samemerald8@gmail.com>
2024-01-12 15:39:46 +0100

    added task

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import List
+""" Basic Annotations """
+
+Flist = List[float]
+
+
+def sum_list(input_list: Flist) -> float:
+    """ basic floor function """
+    return sum(input_list)
