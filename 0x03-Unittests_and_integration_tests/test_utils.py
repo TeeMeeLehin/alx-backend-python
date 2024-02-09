38cb3d8a0edbe5cda6890d0e808a02d467f99e31
TeeMeeLehin <samemerald8@gmail.com>
2024-02-09 20:35:23 +0100

    unit tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """testing the utils module"""
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 import unittest
 from parameterized import parameterized
+from unittest.mock import Mock, patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Test Class"""
+    """ AccessNestedMap Test Class"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -26,3 +27,23 @@ class TestAccessNestedMap(unittest.TestCase):
         """xception test func"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """GetJson Test Class"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """test func"""
+        mock_res = Mock()
+        mock_res.json.return_value = test_payload
+        mock_get.return_value = mock_res
+
+        res = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(res, test_payload)
